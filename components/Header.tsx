c02ec1dfbc56fa973cf1cbdd8b14bf3bf541b951
Steven <chaolic6505@gmail.com>
2022-11-01 21:47:26 -0700

    update package version and add search bar

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,15 +2,12 @@ import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 
-import {
-    HomeIcon,
-    ChevronRightIcon,
-} from '@heroicons/react/24/solid';
-import { BeakerIcon } from '@heroicons/react/24/outline';
+import { HomeIcon, SearchIcon, ChevronDownIcon } from '@heroicons/react/solid';
+import { BeakerIcon } from '@heroicons/react/outline';
 
 function Header() {
     return (
-        <div className='flex bg-white px-4 py-2'>
+        <div className='flex bg-white px-4 py-2 sticky top-0 z-50'>
             <div className='relative h-10 w-20'>
                 <Link href='/'>
                     <Image
@@ -21,12 +18,22 @@ function Header() {
                     />
                 </Link>
             </div>
-
-            <div className='mx-7 flex items-center'>
-                <HomeIcon className='h-5 w-5'/>
-                <p>Home</p>
-                <ChevronRightIcon className='h-5 w-5'/>
+            <div className='mx-7 flex items-center xl:min-w-[300px]'>
+                <HomeIcon className='h-5 w-5' />
+                <p className='ml-2 hidden flex-1 lg:inline'>Home</p>
+                <ChevronDownIcon className='h-5 w-5' />
             </div>
+
+            {/* Searchbox */}
+            <form className='flex flex-1 items-center space-x-2 rounded-sm border border-gray-200 bg-gray-100 px-3 py-1'>
+                <SearchIcon className='h-6 w-6 text-gray-400' />
+                <input
+                    type='text'
+                    placeholder='Search Reddit'
+                    className='flex-1 bg-transparent outline-none'
+                />
+                <button type='submit' hidden />
+            </form>
         </div>
     );
 }
