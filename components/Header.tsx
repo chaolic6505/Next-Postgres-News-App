23c6403aea336a2132efa684c2fb3d52bbb13872
Steven <chaolic6505@gmail.com>
2022-11-01 21:38:06 -0700

    add Home Icon

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,18 +2,30 @@ import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 
+import {
+    HomeIcon,
+    ChevronRightIcon,
+} from '@heroicons/react/24/solid';
+import { BeakerIcon } from '@heroicons/react/24/outline';
+
 function Header() {
     return (
-        <div>
+        <div className='flex bg-white px-4 py-2'>
             <div className='relative h-10 w-20'>
                 <Link href='/'>
                     <Image
                         alt='logo'
                         fill={true}
-                        src="/reddit-logo.png"
+                        src='/reddit-logo.png'
                         className='w-90 cursor-pointer object-contain'
                     />
                 </Link>
+            </div>
+
+            <div className='mx-7 flex items-center'>
+                <HomeIcon className='h-5 w-5'/>
+                <p>Home</p>
+                <ChevronRightIcon className='h-5 w-5'/>
             </div>
         </div>
     );
