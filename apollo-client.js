789cd1fad421c222db577387f9413954e7e07b2a
Steven <chaolic6505@gmail.com>
2022-11-05 05:07:06 -0700

    add graphQL

---
+++ apollo-client.js
@@ -0,0 +1,11 @@
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    uri: 'https://chaudfontaine.stepzen.net/api/awesome-octopus/__graphql',
+    headers: {
+        Authorization: `Apikey ${process.env.STEPZEN_API_KEY}`,
+    },
+});
+
+export default client;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,21 @@
 import '../styles/globals.css';
+import client from '../apollo-client';
 import type { AppProps } from 'next/app';
+import { ApolloProvider } from '@apollo/client';
 import { SessionProvider } from 'next-auth/react';
 
 import Header from '../components/Header';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
     return (
-        <SessionProvider session={session}>
-            <div className='h-screen overflow-y-scroll bg-slate-200'>
-                <Header />
-                <Component {...pageProps} />
-            </div>
-        </SessionProvider>
+        <ApolloProvider client={client}>
+            <SessionProvider session={session}>
+                <div className='h-screen overflow-y-scroll bg-slate-200'>
+                    <Header />
+                    <Component {...pageProps} />
+                </div>
+            </SessionProvider>
+        </ApolloProvider>
     );
 }
 
