d0c469f664b5e8e43eff3b5fe9b9a5ed3783e8a5
Steven <chaolic6505@gmail.com>
2022-11-05 21:09:57 -0800

    update formatting

--- components/Post.tsx
+++ components/Post.tsx
@@ -85,77 +85,100 @@ function Post({ post }: Props) {
         return displayNumber;
     };
 
+    const renderFooter = () => {
+        return (
+            <div className='flex space-x-4 text-gray-400'>
+                <div className='postButtons'>
+                    <ChatAltIcon className='h-6 w-6' />
+                    <p className='hidden sm:inline'>
+                        {post.comments.length} Comments
+                    </p>
+                </div>
+                <div className='postButtons'>
+                    <GiftIcon className='h-6 w-6' />
+                    <p className='hidden sm:inline'>Award</p>
+                </div>
+                <div className='postButtons'>
+                    <ShareIcon className='h-6 w-6' />
+                    <p className='hidden sm:inline'>Share</p>
+                </div>
+                <div className='postButtons'>
+                    <BookmarkIcon className='h-6 w-6' />
+                    <p className='hidden sm:inline'>Save</p>
+                </div>
+                <div className='postButtons'>
+                    <DotsHorizontalIcon className='h-6 w-6' />
+                </div>
+            </div>
+        );
+    };
+
+    const renderVotes = () => {
+        return (
+            <div className='flex flex-col items-center justify-start space-y-1 rounded-l-md bg-white p-4 text-gray-400'>
+                <ArrowUpIcon
+                    onClick={() => upVote(true)}
+                    className={`voteButtons hover:text-blue-500 ${
+                        vote && 'text-blue-500'
+                    }`}
+                />
+                <p className='text-xs font-bold text-black'>
+                    {displayVotes(data)}
+                </p>
+                <ArrowDownIcon
+                    onClick={() => upVote(false)}
+                    className={`voteButtons hover:text-red-400 ${
+                        vote === false && 'text-red-400'
+                    }`}
+                />
+            </div>
+        );
+    };
+
+    const renderHeader = () => {
+        return (
+            <div className='flex items-center space-x-2'>
+                <Avatar seed={post.subreddit[0]?.topic} />
+                <p className='text-xs text-gray-400'>
+                    <Link href={`/subreddit/${post.subreddit[0]?.topic}`}>
+                        <span className='font-bold text-black hover:text-blue-400 hover:underline'>
+                            r/{post.subreddit[0]?.topic}
+                        </span>
+                    </Link>{' '}
+                    - Posted by u/
+                    {post.username} <TimeAgo date={post.created_at} />
+                </p>
+            </div>
+        );
+    };
+
+    const renderPostImage = () => {
+        return <img className='w-full' src={post.image} alt='' />;
+    };
+
+    const renderBody = () => {
+        return (
+            <div className='py-4'>
+                <h2 className='text-xl font-semibold'>{post.title}</h2>
+                <p className='mt-2 text-sm font-light'>{post.body}</p>
+            </div>
+        );
+    };
+
     return (
         <Link href={`/post/${post.id}`}>
-            <div className='flex cursor-pointer bg-white'>
+            <div className='flex cursor-pointer bg-white rounded-md border border-gray-300 shadow-sm hover:border hover:border-gray-600'>
                 {/* Votes */}
-                <div className='flex flex-col items-center justify-start space-y-1 rounded-l-md bg-white p-4 text-gray-400'>
-                    <ArrowUpIcon
-                        onClick={() => upVote(true)}
-                        className={`voteButtons hover:text-blue-500 ${
-                            vote && 'text-blue-500'
-                        }`}
-                    />
-                    <p className='text-xs font-bold text-black'>
-                        {displayVotes(data)}
-                    </p>
-                    <ArrowDownIcon
-                        onClick={() => upVote(false)}
-                        className={`voteButtons hover:text-red-400 ${
-                            vote === false && 'text-red-400'
-                        }`}
-                    />
-                </div>
+                {renderVotes()}
                 <div className='p-3 pb-1 '>
                     {/* Header */}
-                    <div className='flex items-center space-x-2'>
-                        <Avatar seed={post.subreddit[0]?.topic} />
-                        <p className='text-xs text-gray-400'>
-                            <Link
-                                href={`/subreddit/${post.subreddit[0]?.topic}`}
-                            >
-                                <span className='font-bold text-black hover:text-blue-400 hover:underline'>
-                                    r/{post.subreddit[0]?.topic}
-                                </span>
-                            </Link>{' '}
-                            - Posted by u/
-                            {post.username} <TimeAgo date={post.created_at} />
-                        </p>
-                    </div>
-
+                    {renderHeader()}
                     {/* Body */}
-                    <div className='py-4'>
-                        <h2 className='text-xl font-semibold'>{post.title}</h2>
-                        <p className='mt-2 text-sm font-light'>{post.body}</p>
-                    </div>
-
+                    {renderBody()}
                     {/* Image */}
-                    <img className='w-full' src={post.image} alt='' />
-
+                    {renderPostImage()}
                     {/* Footer */}
-                    <div className='flex space-x-4 text-gray-400'>
-                        <div className='postButtons'>
-                            <ChatAltIcon className='h-6 w-6' />
-                            <p className='hidden sm:inline'>
-                                {post.comments.length} Comments
-                            </p>
-                        </div>
-                        <div className='postButtons'>
-                            <GiftIcon className='h-6 w-6' />
-                            <p className='hidden sm:inline'>Award</p>
-                        </div>
-                        <div className='postButtons'>
-                            <ShareIcon className='h-6 w-6' />
-                            <p className='hidden sm:inline'>Share</p>
-                        </div>
-                        <div className='postButtons'>
-                            <BookmarkIcon className='h-6 w-6' />
-                            <p className='hidden sm:inline'>Save</p>
-                        </div>
-                        <div className='postButtons'>
-                            <DotsHorizontalIcon className='h-6 w-6' />
-                        </div>
-                    </div>
+                    {renderFooter()}
                 </div>
             </div>
         </Link>
