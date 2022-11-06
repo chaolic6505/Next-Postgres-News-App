27dd0ce7810da4a028520f21a6af99eef0a520bb
Steven <chaolic6505@gmail.com>
2022-11-05 19:31:59 -0800

    create/ fetch post works

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,19 @@
 import '../styles/globals.css';
 import client from '../apollo-client';
 import type { AppProps } from 'next/app';
+import { Toaster } from 'react-hot-toast';
 import { ApolloProvider } from '@apollo/client';
 import { SessionProvider } from 'next-auth/react';
 
 import Header from '../components/Header';
-import Postbox from '../components/Postbox';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
     return (
         <ApolloProvider client={client}>
             <SessionProvider session={session}>
+                <Toaster />
                 <div className='h-screen overflow-y-scroll bg-slate-200'>
                     <Header />
-                    <Postbox />
                     <Component {...pageProps} />
                 </div>
             </SessionProvider>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,22 @@
-import type { NextPage } from 'next';
 import Head from 'next/head';
+import type { NextPage } from 'next';
+
+import Feed from '../components/Feed';
+import Postbox from '../components/Postbox';
 
 const Home: NextPage = () => {
     return (
-        <Head>
-            <title>Reddit Clone</title>
-        </Head>
+        <div className='my-7 mx-auto max-w-5xl'>
+            <Head>
+                <title>Reddit 2.0 Clone</title>
+            </Head>
+
+            <Postbox />
+
+            <div className='flex'>
+                <Feed />
+            </div>
+        </div>
     );
 };
 
