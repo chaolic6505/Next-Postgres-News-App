e19fe9681f062c0923b5f70be2b15494db0f0919
Steven <chaolic6505@gmail.com>
2022-11-05 19:55:40 -0800

    add subreddit component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,17 @@
 import Head from 'next/head';
 import type { NextPage } from 'next';
+import { useQuery } from '@apollo/client';
 
 import Feed from '../components/Feed';
 import Postbox from '../components/Postbox';
+import SubredditRow from '../components/SubredditRow';
+import { GET_SUBREDDITS_WITH_LIMIT } from '../graphql/queries';
 
 const Home: NextPage = () => {
+    const { data } = useQuery(GET_SUBREDDITS_WITH_LIMIT);
+
+    const subreddits: Subreddit[] = data?.getSubredditListLimit;
+
     return (
         <div className='my-7 mx-auto max-w-5xl'>
             <Head>
@@ -15,6 +22,20 @@ const Home: NextPage = () => {
 
             <div className='flex'>
                 <Feed />
+                <div className='sticky top-36 ml-5 mt-5 hidden h-fit min-w-[300px] rounded-md bg-white lg:inline'>
+                    <p className='text-md mb-1 p-4 pb-3 font-bold'>
+                        Top Communities
+                    </p>
+                    <div>
+                        {subreddits?.map((subreddit, i) => (
+                            <SubredditRow
+                                index={i}
+                                key={subreddit.id}
+                                topic={subreddit.topic}
+                            />
+                        ))}
+                    </div>
+                </div>
             </div>
         </div>
     );
