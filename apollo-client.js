9bbb061dcb0567639772fbb64fb2b5e034db1fed
chaolic6505 <chaolic6505@gmail.com>
2022-12-05 22:10:01 -0800

    update

--- apollo-client.js
+++ apollo-client.js
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  uri: 'https://chaudfontaine.stepzen.net/api/awesome-octopus/',
+  uri: 'https://chaudfontaine.stepzen.net/api/awesome-octopus/__graphql',
   headers: {
     Authorization: `Apikey ${process.env.STEPZEN_API_KEY}`,
   },
